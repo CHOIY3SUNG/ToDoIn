980921092e55da05ad7790bb1c0069b41e820ef7
CHOIY3SUNG <choiyesung2848@naver.com>
2022-08-08 01:59:18 +0900

    commit

---
+++ todoin/todoin/todoinApp.swift
@@ -0,0 +1,20 @@
+//
+//  todoinApp.swift
+//  todoin
+//
+//  Created by Y3SUNG on 2022/08/08.
+//
+
+import SwiftUI
+
+@main
+struct todoinApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
